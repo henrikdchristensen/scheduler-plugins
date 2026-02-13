e0518a041b502ce620ee1043a75ebd0b97e837ad
henrikdchristensen <henrikdchristensen@gmail.com>
2026-02-13 10:31:13 +0100

    up

--- analysis/kwok_workload_once/cleanup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Recursively remove files starting with "eta_" or "seeds-", and files named "failed.csv"
-
-find . -type f \( -name "eta_*" -o -name "seeds-*" -o -name "failed.csv" \) -print -delete
